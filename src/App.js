c4191bf3d9ccb57f63ab6973e6f4ea814fffb8ad
Raj Sagar <therajsagar@gmail.com>
2020-01-18 18:04:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import QrReader from "react-qr-reader";
+import "./App.css";
 
 function App() {
+  const handleScan = data => {
+    if (data) {
+      console.log(data);
+    }
+  };
+
+  const handleError = err => {
+    console.error(err);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Please Scan!</h1>
+      <QrReader
+        delay={200}
+        onError={handleError}
+        onScan={handleScan}
+        style={{ width: "100%" }}
+      />
     </div>
   );
 }
