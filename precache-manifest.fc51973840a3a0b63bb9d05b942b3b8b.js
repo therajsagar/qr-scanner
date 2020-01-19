d9a358eef3fefe7ec7f76ef14ad649de8a900932
Raj Sagar <therajsagar@gmail.com>
2020-01-19 18:58:31 +0530

    Updates

---
+++ precache-manifest.fc51973840a3a0b63bb9d05b942b3b8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dadf22bc6066b52f3576fc415dceff6",
+    "url": "/qr-scanner/index.html"
+  },
+  {
+    "revision": "c93006f2e7942d0eb8fb",
+    "url": "/qr-scanner/static/css/main.3b21b705.chunk.css"
+  },
+  {
+    "revision": "e4f327cd31369fb4ee45",
+    "url": "/qr-scanner/static/js/2.1bdf44cd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/qr-scanner/static/js/2.1bdf44cd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c93006f2e7942d0eb8fb",
+    "url": "/qr-scanner/static/js/main.9e9975ee.chunk.js"
+  },
+  {
+    "revision": "498c614f004650bf4185",
+    "url": "/qr-scanner/static/js/runtime-main.2379b821.js"
+  }
+]);
